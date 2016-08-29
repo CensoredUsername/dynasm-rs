bc989ab1dd55e5d9621e9c33f5b6b10012f87cd6
CensoredUsername <cens.username@gmail.com>
2016-08-29 21:59:42 +0200

    Move the example hello world project to its own folder, add a README to the examples folder and update the package metadata for the plugin and the runtime

---
+++ doc/examples/README.md
@@ -0,0 +1,1 @@
+This folder contains several example dynasm-rs based projects. Each of them can be executed with the normal `cargo run`.
---
+++ doc/examples/hello-world/src/main.rs
@@ -0,0 +1,48 @@
+#![feature(plugin)]
+#![plugin(dynasm)]
+
+#[macro_use]
+extern crate dynasmrt;
+
+use dynasmrt::DynasmApi;
+
+use std::{io, slice, mem};
+use std::io::Write;
+
+fn main() {
+    let mut ops = dynasmrt::Assembler::new();
+    let string = "Hello World!";
+
+    dynasm!(ops
+        ; ->hello:
+        ; .bytes string.as_bytes()
+    );
+
+    let hello = ops.offset();
+    dynasm!(ops
+        ; lea rcx, [->hello]
+        ; xor edx, edx
+        ; mov dl, BYTE string.len() as _
+        ; mov rax, QWORD print as _
+        ; sub rsp, BYTE 0x28
+        ; call rax
+        ; add rsp, BYTE 0x28
+        ; ret
+    );
+
+    let buf = ops.finalize().unwrap();
+
+    let hello_fn: extern "win64" fn() -> bool = unsafe {
+        mem::transmute(buf.ptr(hello))
+    };
+
+    assert!(
+        hello_fn()
+    );
+}
+
+pub extern "win64" fn print(buffer: *const u8, length: u64) -> bool {
+    io::stdout().write_all(unsafe {
+        slice::from_raw_parts(buffer, length as usize)
+    }).is_ok()
+}
