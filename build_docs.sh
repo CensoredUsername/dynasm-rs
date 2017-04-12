cbbd7c537dce37049885ba43bdfba2e17acd8daf
CensoredUsername <cens.username@gmail.com>
2017-04-12 17:28:12 +0200

    Update build script to print status updates

--- build_docs.sh
+++ build_docs.sh
@@ -4,37 +4,37 @@ set -o errexit
 
 shopt -s globstar
 
-# build individual documentation
+echo "build individual documentation"
 (cd plugin && cargo doc)
 (cd runtime && cargo doc)
 
-# remove old docs build
+echo "remove old docs build"
 rm -rf build_docs
 
-# build directory structure
+echo "build directory structure"
 mkdir ./build_docs
 mkdir ./build_docs/language
 mkdir ./build_docs/plugin
 mkdir ./build_docs/runtime
 
-# create instruction reference markdown file
+echo "create instruction reference markdown file"
 (cd doc/insref && cargo update && cargo run > ../instructionref.md)
 
-# build plugin docs
+echo "build plugin docs"
 for f in ./doc/*.md; do
     rustdoc $f -o ./build_docs/language --markdown-no-toc --html-before-content=./doc/pre.html --html-after-content=./doc/post.html --markdown-css=./formatting.css
 done
 cp ./doc/formatting.css ./build_docs/language/formatting.css
 
-# copy over the docs folders
+echo "copy over the docs folders"
 cp -r ./plugin/target/doc/* ./build_docs/plugin
 cp -r ./runtime/target/doc/* ./build_docs/runtime
 
-# hack in javascript
+echo "insert javascript"
 cat ./doc/hack.js >> ./build_docs/plugin/search-index.js
 cat ./doc/hack.js >> ./build_docs/runtime/search-index.js
 
-# copy docs examples to tests
+echo "copy docs examples to tests"
 declare -a examples=("bf-interpreter" "bf-jit" "hello-world")
 for EX in "${examples[@]}"
 do
@@ -45,7 +45,9 @@ do
 done
 
 if [ "$1" == "commit" ]; then
+    echo "cloning gh-pages into a temporary directory"
     git clone --branch gh-pages --depth 1 "git@github.com:CensoredUsername/dynasm-rs.git" deploy_docs
+
     cd deploy_docs
     git config user.name "CensoredUsername"
     git config user.email "cens.username@gmail.com"
