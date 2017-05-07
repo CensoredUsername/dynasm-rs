46b19001ca912415bf08a1cc7c98f6337ee32776
CensoredUsername <cens.username@gmail.com>
2017-05-07 15:36:10 +0200

    Rebuild docs

---
+++ plugin/implementors/stable_deref_trait/trait.CloneStableDeref.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["dynasm"] = ["impl&lt;O, T&gt; CloneStableDeref for OwningRef&lt;O, T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;O: CloneStableDeref,<br>&nbsp;&nbsp;&nbsp;&nbsp;T: ?<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a>,&nbsp;</span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
