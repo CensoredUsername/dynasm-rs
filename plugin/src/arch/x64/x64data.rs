4af681cda2b6ea0cce3e26b2d3cdb44d4b648431
CensoredUsername <cens.username@gmail.com>
2017-06-07 02:01:30 +0200

    Upgrade bitflags to 0.9.1

--- plugin/src/arch/x64/x64data.rs
+++ plugin/src/arch/x64/x64data.rs
@@ -38,36 +38,36 @@ pub fn get_mnemnonic_data(name: &str) -> Option<&'static [Opdata]> {
 
 pub mod flags {
     bitflags! {
-        pub flags Flags: u32 {
-            const DEFAULT   = 0x0000_0000, // this instruction has default encoding
-            const VEX_OP    = 0x0000_0001, // this instruction requires a VEX prefix to be encoded
-            const XOP_OP    = 0x0000_0002, // this instruction requires a XOP prefix to be encoded
-            const IMM_OP    = 0x0000_0004, // this instruction encodes the final opcode byte in the immediate position, like 3DNow! ops.
+        pub struct Flags: u32 {
+            const DEFAULT   = 0x0000_0000; // this instruction has default encoding
+            const VEX_OP    = 0x0000_0001; // this instruction requires a VEX prefix to be encoded
+            const XOP_OP    = 0x0000_0002; // this instruction requires a XOP prefix to be encoded
+            const IMM_OP    = 0x0000_0004; // this instruction encodes the final opcode byte in the immediate position, like 3DNow! ops.
 
             // note: the first 4 in this block are mutually exclusive
-            const AUTO_SIZE = 0x0000_0008, // 16 bit -> OPSIZE , 32-bit -> None   , 64-bit -> REX.W/VEX.W/XOP.W
-            const AUTO_NO32 = 0x0000_0010, // 16 bit -> OPSIZE , 32-bit -> illegal, 64-bit -> None
-            const AUTO_REXW = 0x0000_0020, // 16 bit -> illegal, 32-bit -> None   , 64-bit -> REX.W/VEX.W/XOP.W
-            const AUTO_VEXL = 0x0000_0040, // 128bit -> None   , 256bit -> VEX.L
-            const WORD_SIZE = 0x0000_0080, // implies opsize prefix
-            const WITH_REXW = 0x0000_0100, // implies REX.W/VEX.W/XOP.W
-            const WITH_VEXL = 0x0000_0200, // implies VEX.L/XOP.L
-            const EXACT_SIZE= 0x0000_0400, // operands with unknown sizes cannot be assumed to match
+            const AUTO_SIZE = 0x0000_0008; // 16 bit -> OPSIZE , 32-bit -> None   , 64-bit -> REX.W/VEX.W/XOP.W
+            const AUTO_NO32 = 0x0000_0010; // 16 bit -> OPSIZE , 32-bit -> illegal, 64-bit -> None
+            const AUTO_REXW = 0x0000_0020; // 16 bit -> illegal, 32-bit -> None   , 64-bit -> REX.W/VEX.W/XOP.W
+            const AUTO_VEXL = 0x0000_0040; // 128bit -> None   , 256bit -> VEX.L
+            const WORD_SIZE = 0x0000_0080; // implies opsize prefix
+            const WITH_REXW = 0x0000_0100; // implies REX.W/VEX.W/XOP.W
+            const WITH_VEXL = 0x0000_0200; // implies VEX.L/XOP.L
+            const EXACT_SIZE= 0x0000_0400; // operands with unknown sizes cannot be assumed to match
 
-            const PREF_66   = WORD_SIZE.bits,// mandatory prefix (same as WORD_SIZE)
-            const PREF_67   = 0x0000_0800, // mandatory prefix (same as SMALL_ADDRESS)
-            const PREF_F0   = 0x0000_1000, // mandatory prefix (same as LOCK)
-            const PREF_F2   = 0x0000_2000, // mandatory prefix (REPNE)
-            const PREF_F3   = 0x0000_4000, // mandatory prefix (REP)
+            const PREF_66   = WORD_SIZE.bits;// mandatory prefix (same as WORD_SIZE)
+            const PREF_67   = 0x0000_0800; // mandatory prefix (same as SMALL_ADDRESS)
+            const PREF_F0   = 0x0000_1000; // mandatory prefix (same as LOCK)
+            const PREF_F2   = 0x0000_2000; // mandatory prefix (REPNE)
+            const PREF_F3   = 0x0000_4000; // mandatory prefix (REP)
 
-            const LOCK      = 0x0000_8000, // user lock prefix is valid with this instruction
-            const REP       = 0x0001_0000, // user rep prefix is valid with this instruction
-            const REPE      = 0x0002_0000,
+            const LOCK      = 0x0000_8000; // user lock prefix is valid with this instruction
+            const REP       = 0x0001_0000; // user rep prefix is valid with this instruction
+            const REPE      = 0x0002_0000;
 
-            const SHORT_ARG = 0x0004_0000, // a register argument is encoded in the last byte of the opcode
-            const ENC_MR    = 0x0008_0000, // select alternate arg encoding
-            const ENC_VM    = 0x0010_0000, // select alternate arg encoding
-            const ENC_MIB   = 0x0020_0000, // A special encoding using the SIB to specify an immediate and two registers
+            const SHORT_ARG = 0x0004_0000; // a register argument is encoded in the last byte of the opcode
+            const ENC_MR    = 0x0008_0000; // select alternate arg encoding
+            const ENC_VM    = 0x0010_0000; // select alternate arg encoding
+            const ENC_MIB   = 0x0020_0000; // A special encoding using the SIB to specify an immediate and two registers
         }
     }
 
@@ -82,33 +82,33 @@ pub mod flags {
 
 pub mod features {
     bitflags! {
-        pub flags Features: u32 {
-            const X64_IMPLICIT = 0x0000_0000,
-            const FPU          = 0x0000_0001,
-	          const MMX          = 0x0000_0002,
-            const TDNOW        = 0x0000_0004,
-            const SSE          = 0x0000_0008,
-            const SSE2         = 0x0000_0010,
-            const SSE3         = 0x0000_0020,
-            const VMX          = 0x0000_0040,
-            const SSSE3        = 0x0000_0080,
-            const SSE4A        = 0x0000_0100,
-            const SSE41        = 0x0000_0200,
-            const SSE42        = 0x0000_0400,
-            const SSE5         = 0x0000_0800,
-            const AVX          = 0x0000_1000,
-            const AVX2         = 0x0000_2000,
-            const FMA          = 0x0000_4000,
-            const BMI1         = 0x0000_8000,
-            const BMI2         = 0x0001_0000,
-            const TBM          = 0x0002_0000,
-            const RTM          = 0x0004_0000,
-            const INVPCID      = 0x0008_0000,
-            const MPX          = 0x0010_0000,
-            const SHA          = 0x0020_0000,
-            const PREFETCHWT1  = 0x0040_0000,
-            const CYRIX        = 0x0080_0000,
-            const AMD          = 0x0100_0000,
+        pub struct Features: u32 {
+            const X64_IMPLICIT = 0x0000_0000;
+            const FPU          = 0x0000_0001;
+	          const MMX          = 0x0000_0002;
+            const TDNOW        = 0x0000_0004;
+            const SSE          = 0x0000_0008;
+            const SSE2         = 0x0000_0010;
+            const SSE3         = 0x0000_0020;
+            const VMX          = 0x0000_0040;
+            const SSSE3        = 0x0000_0080;
+            const SSE4A        = 0x0000_0100;
+            const SSE41        = 0x0000_0200;
+            const SSE42        = 0x0000_0400;
+            const SSE5         = 0x0000_0800;
+            const AVX          = 0x0000_1000;
+            const AVX2         = 0x0000_2000;
+            const FMA          = 0x0000_4000;
+            const BMI1         = 0x0000_8000;
+            const BMI2         = 0x0001_0000;
+            const TBM          = 0x0002_0000;
+            const RTM          = 0x0004_0000;
+            const INVPCID      = 0x0008_0000;
+            const MPX          = 0x0010_0000;
+            const SHA          = 0x0020_0000;
+            const PREFETCHWT1  = 0x0040_0000;
+            const CYRIX        = 0x0080_0000;
+            const AMD          = 0x0100_0000;
         }
     }
 
