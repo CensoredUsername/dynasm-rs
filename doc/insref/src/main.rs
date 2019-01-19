4b2d9575c3579342f22d80893d2e304ab82aedbc
CensoredUsername <cens.username@gmail.com>
2019-01-19 01:08:43 +0100

    Port the instruction reference documentation project to proc_macro as well

--- doc/insref/src/main.rs
+++ doc/insref/src/main.rs
@@ -1,10 +1,9 @@
-#![feature(plugin)]
-#![plugin(dynasm)]
+#![feature(proc_macro_hygiene)]
 
 use std::io::{self, Write};
 
 fn main() {
     let stdout = io::stdout();
     let mut stdout = stdout.lock();
-    stdout.write_all(dynasm_opmap!().as_bytes()).unwrap();
+    stdout.write_all(dynasm::dynasm_opmap!().as_bytes()).unwrap();
 }
