3d4de3502233212dd0885ef9a4662feb569342d9
CensoredUsername <cens.username@gmail.com>
2019-08-22 12:02:22 +0000

    beta version integration tests

--- tools/aarch64_compile_tests.py
+++ tools/aarch64_compile_tests.py
@@ -4,16 +4,20 @@ import binascii
 def read_test_strings(f):
     buf = []
     for line in f:
-        dynasm, gas = line.split("\t")
-        buf.append((dynasm, gas))
+        if line:
+            if not "\t" in line:
+                print(line)
+            dynasm, gas = line.split("\t")
+            buf.append((dynasm.strip(), gas.strip()))
 
     return buf
 
 def compile_with_as(asmstring):
     with open("test.s", "w", encoding="utf-8") as f:
         f.write(asmstring)
+        f.write("\n")
 
-    subprocess.run(["as", "test.s", "-o", "test.o"], check=True)
+    subprocess.run(["as", "-mcpu=all", "test.s", "-o", "test.o"], check=True)
     subprocess.run(["objcopy", "-O", "binary", "test.o", "test.bin"], check=True)
 
     with open("test.bin", "rb") as f:
@@ -21,23 +25,23 @@ def compile_with_as(asmstring):
     return data
 
 def write_result(buf, f):
-    for dynasm, gas, binary in f:
-        f.write("{}\t{}\t{}".format(dynasm, gas, binascii.hexlify(binary)))
+    for dynasm, gas, binary in buf:
+        f.write("{}\t{}\t{}\n".format(dynasm, gas, binascii.hexlify(binary).decode("utf-8")))
 
 def main():
     import sys
-    test_strings = read_test_strings(sys.argv[1])
+    with open(sys.argv[1], "r", encoding="utf-8") as f:
+        test_strings = read_test_strings(f)
 
     buf = []
     for dynasm, gas in test_strings:
         try:
             binary = compile_with_as(gas)
+            buf.append((dynasm, gas, binary))
         except:
-            Print("Error at {}".format(gas))
-            raise
-        buf.append((dynasm, gas, binary))
+            print("Error at {}".format(gas))
 
-    with open(sys.argv[2]):
+    with open(sys.argv[2], "w", encoding="utf-8") as f:
         write_result(buf, f)
 
 if __name__ == '__main__':
---
+++ tools/aarch64_emit_tests.py
@@ -0,0 +1,48 @@
+import os
+import os.path
+
+def read_input_file(f):
+    buf = []
+    for line in f:
+        dynasm, gas, bytes = line.split("\t")
+        buf.append((dynasm.strip(), gas.strip(), bytes.strip()))
+    return buf
+
+def chunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i+n]
+
+def main():
+    import sys
+    with open(sys.argv[1], "r", encoding="utf-8") as f:
+        data = read_input_file(f)
+
+    tests = [emit_test_case(i, dynasm, gas, bytes) for i, (dynasm, gas, bytes) in enumerate(data)]
+
+    for i, chunk in enumerate(chunks(tests, 1000)):
+        with open(os.path.join(sys.argv[2], "aarch64_tests_{}.rs.gen".format(i)), "w", encoding="utf-8") as f:
+            for test in chunk:
+                f.write(test)
+
+def emit_test_case(i, dynasm, gas, bytes):
+    name = dynasm.split(' ', 1)[0].replace(".", "_")
+    bytes = ", ".join(chunks(bytes, 2)).upper()
+    return """
+#[test]
+fn {}_{}() {{
+    let mut ops = dynasmrt::aarch64::Assembler::new().unwrap();
+    dynasm!(ops
+        ; .arch aarch64
+        ; {}
+    );
+    let buf = ops.finalize().unwrap();
+    let hex: Vec<String> = buf.iter().map(|x| format!("{{:02X}}", *x)).collect();
+    let hex = hex.join(", ");
+    assert_eq(hex, "{}", "{}");
+}}
+""".format(name, i, dynasm, bytes, dynasm)
+
+
+
+if __name__ == "__main__":
+    main()
