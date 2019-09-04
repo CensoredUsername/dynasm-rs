b2d49cf930d5d4c51cb62e019a8edbc260aa594c
CensoredUsername <cens.username@gmail.com>
2019-09-05 01:52:18 +0200

    Rebuild docs

---
+++ runtime/implementors/core/ops/deref/trait.DerefMut.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["dynasmrt"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"dynasmrt/mmap/struct.MutableBuffer.html\" title=\"struct dynasmrt::mmap::MutableBuffer\">MutableBuffer</a>",synthetic:false,types:["dynasmrt::mmap::MutableBuffer"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
--- runtime/source-files.js
+++ runtime/source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["dynasmrt"] = {"name":"","files":["aarch64.rs","common.rs","lib.rs","x64.rs","x86.rs"]};
+sourcesIndex["dynasmrt"] = {"name":"","files":["aarch64.rs","components.rs","lib.rs","mmap.rs","relocations.rs","x64.rs","x86.rs"]};
 createSourceSidebar();
