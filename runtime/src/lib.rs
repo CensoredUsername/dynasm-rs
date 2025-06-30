939f7338b661627713a58c0de3f94a0db6fbb525
nhtyy <n@push0.dev>
2025-06-30 14:31:11 -0700

    fix: memory should be a multiple of pagesize, api should use bytes for capacity

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -583,16 +583,25 @@ impl<R: Relocation> Assembler<R> {
         })
     }
 
-    /// Create a new assembler with a pre-allocated buffer of the specified capacity.
+    /// Create a new, empty assembler, with a pre-allocated buffer of the specified capacity in bytes,
+    /// and an initial allocation of `capacity.next_multiple_of(page_size)`.
     pub fn new_with_capacity(capacity: usize) -> io::Result<Self> {
-        // Scale the amount by 7 to account for the average encoding size, in bytes.
+        // Avoid panic or wraps on overflow.
+        let memory_size =
+            capacity
+                .checked_next_multiple_of(R::page_size())
+                .ok_or(io::Error::new(
+                    io::ErrorKind::InvalidInput,
+                    "Capacity is too large",
+                ))?;
+
         Ok(Self {
-            ops: Vec::with_capacity(capacity * 7),
-            memory: MemoryManager::new(capacity * 7)?,
+            ops: Vec::with_capacity(capacity),
+            memory: MemoryManager::new(memory_size)?,
             labels: LabelRegistry::new(),
             relocs: RelocRegistry::new(),
             managed: ManagedRelocs::new(),
-            error: None
+            error: None,
         })
     }
 
